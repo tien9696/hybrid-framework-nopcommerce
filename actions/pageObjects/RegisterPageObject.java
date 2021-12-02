a02b279dcd3039288f7adf600265fdec7e0b54b7
Automation testing <Themf_000@192.168.1.9>
2021-12-02 22:37:59 +0700

    Update RegisterPageObject

--- actions/pageObjects/RegisterPageObject.java
+++ actions/pageObjects/RegisterPageObject.java
@@ -9,14 +9,18 @@ public class RegisterPageObject extends BasePage {
 
 	private WebDriver driver;
 	
-	public RegisterPageObject(WebDriver driver) {
-		this.driver = driver;
-	}
+//	public RegisterPageObject(WebDriver driver) {
+//		this.driver = driver;
+//	}
 
 	public void clickToRegisterButton() {
 		waitForElementClickable(driver, RegisterPageUI.REGISTER_BUTTON);
 		clickToElement(driver, RegisterPageUI.REGISTER_BUTTON);
 
+	}
+
+	public RegisterPageObject(WebDriver driver) {
+		this.driver = driver;
 	}
 
 	public String getErrorMessageAtFirstnameTextbox() {
