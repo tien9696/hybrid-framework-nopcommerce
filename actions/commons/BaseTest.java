056619caceff5ab8a2850af49c13d31d3c09d032
Automation testing <Themf_000@192.168.1.9>
2021-12-04 22:50:50 +0700

    Multiple Browser

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,36 @@
 package commons;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BaseTest {
+
+	private WebDriver driverBaseTest;
+	private String projectPath = System.getProperty("user.dir");
+
+	protected WebDriver getBrowserDriver(String browserName) {
+		if (browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\BrowserDriver\\geckodriver.exe");
+			driverBaseTest = new FirefoxDriver();
+		} else if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\BrowserDriver\\chromedriver.exe");
+			driverBaseTest = new ChromeDriver();
+		} else if (browserName.equals("edge")) {
+			System.setProperty("webdriver.edge.driver", projectPath + "\\BrowserDriver\\msedgedriver.exe");
+			driverBaseTest = new EdgeDriver();
+
+		} else {
+			throw new RuntimeException("Browser name invalid");
+		}
+		driverBaseTest.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+		driverBaseTest.get("https://demo.nopcommerce.com/");
+
+		return driverBaseTest;
+	}
 
 }
---
+++ testcases/com/nopcommerce/user/Level_04_Multiple_Browser.java
@@ -0,0 +1,78 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+
+import commons.BasePage;
+import commons.BaseTest;
+import pageObjects.HomePageObject;
+import pageObjects.RegisterPageObject;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Level_04_Multiple_Browser extends BaseTest {
+	// BasePage: Class
+	// basePage: Object
+
+	private WebDriver driverTestClass;
+	private String firstName, lastName, emailAddress, password;
+	private HomePageObject homePage;
+	private RegisterPageObject registerPage;
+
+	@Parameters("browser")
+	@BeforeClass
+	public void beforeClass(String browserName) {
+		// gọi đến cái khởi tạo driver tương ứng với browser name
+		System.out.println("Run on " + browserName);
+
+		driverTestClass = getBrowserDriver(browserName);
+
+		homePage = new HomePageObject(driverTestClass);
+		firstName = "Automation";
+		lastName = "Fc";
+		password = "123456";
+		emailAddress = "afc" + generateFakeNumber() + "@gmail.com";
+	}
+
+	@Test
+	public void Register_01_Empty_Data() {
+		System.out.println("Register_01 - Steps: 01 click to register link");
+		homePage.clickToRegisterLink();
+		// click register link > nhảy qua trang register
+		registerPage = new RegisterPageObject(driverTestClass);
+
+		System.out.println("Register_01 - Steps: 02 click to register link");
+		registerPage.clickToRegisterButton();
+
+		System.out.println("Register_01 - Steps: 03 Verify error message displayed");
+
+		Assert.assertEquals(registerPage.getErrorMessageAtFirstnameTextbox(), "First name is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtLasttnameTextbox(), "Last name is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtEmailTextbox(), "Email is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtPasswordTextbox(), "Password is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtConfirmPasswordTextbox(), "Password is required.");
+	}
+
+
+	@AfterClass
+	public void afterClass() {
+		driverTestClass.quit();
+	}
+
+	public int generateFakeNumber() {
+		Random rand = new Random();
+		return rand.nextInt(9999);
+	}
+
+}
