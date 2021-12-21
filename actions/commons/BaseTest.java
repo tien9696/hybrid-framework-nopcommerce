fb07cbeb8e4339f5dcc205ca75a85a4c3720f5af
Automation testing <Themf_000@192.168.1.9>
2021-12-21 21:44:54 +0700

    Assert vs Vesify

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -2,11 +2,15 @@ package commons;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import javaOOP.BrowserList;
@@ -16,6 +20,12 @@ public class BaseTest {
 	
 	
 	private WebDriver driverBaseTest;
+	
+	protected final Log log;
+	protected BaseTest() {
+		log = LogFactory.getLog(getClass());
+	}
+	
 	private String projectPath = System.getProperty("user.dir");
 
 	protected WebDriver getBrowserDriver(String browserName) {
@@ -40,4 +50,66 @@ public class BaseTest {
 		return driverBaseTest;
 	}
 
+	private boolean checkTrue(boolean condition) {
+		boolean pass = true;
+		try {
+			if (condition == true) {
+				log.info(" -------------------------- PASSED -------------------------- ");
+			} else {
+				log.info(" -------------------------- FAILED -------------------------- ");
+			}
+			Assert.assertTrue(condition);
+		} catch (Throwable e) {
+			pass = false;
+
+			// Add lỗi vào ReportNG
+			VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+			Reporter.getCurrentTestResult().setThrowable(e);
+		}
+		return pass;
+	}
+
+	protected boolean verifyTrue(boolean condition) {
+		return checkTrue(condition);
+	}
+
+	private boolean checkFailed(boolean condition) {
+		boolean pass = true;
+		try {
+			if (condition == false) {
+				log.info(" -------------------------- PASSED -------------------------- ");
+			} else {
+				log.info(" -------------------------- FAILED -------------------------- ");
+			}
+			Assert.assertFalse(condition);
+		} catch (Throwable e) {
+			pass = false;
+			VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+			Reporter.getCurrentTestResult().setThrowable(e);
+		}
+		return pass;
+	}
+
+	protected boolean verifyFalse(boolean condition) {
+		return checkFailed(condition);
+	}
+
+	private boolean checkEquals(Object actual, Object expected) {
+		boolean pass = true;
+		try {
+			Assert.assertEquals(actual, expected);
+			log.info(" -------------------------- PASSED -------------------------- ");
+		} catch (Throwable e) {
+			pass = false;
+			log.info(" -------------------------- FAILED -------------------------- ");
+			VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+			Reporter.getCurrentTestResult().setThrowable(e);
+		}
+		return pass;
+	}
+
+	protected boolean verifyEquals(Object actual, Object expected) {
+		return checkEquals(actual, expected);
+	}
+	
 }
---
+++ testcases/com/facebook/register/Level_12_Register_Login_Assert_Verify.java
@@ -0,0 +1,75 @@
+package com.facebook.register;
+
+import org.testng.annotations.Test;
+
+import commons.BasePage;
+import commons.BaseTest;
+import commons.PageGeneratorManagernopcommerce;
+import pageObject.nopCommerce.admin.AdminDashBoardPageObject;
+import pageObject.nopCommerce.admin.AdminLoginPageObject;
+import pageObject.nopCommerce.admin.AdminProductDetailsPageObject;
+import pageObject.nopCommerce.admin.AdminProductSearchPageObject;
+import pageObjectFacebook.PageGeneratorManager;
+import pageObjectFacebook.RegisterPageObject;
+import pageObjects.nopCommerce.user.UserAddressPageObject;
+import pageObjects.nopCommerce.user.UserHomePageObject;
+import pageObjects.nopCommerce.user.UserLoginPageObject;
+import pageObjects.nopCommerce.user.UserMyAccountPageObject;
+import pageObjects.nopCommerce.user.UserOdersPageObject;
+import pageObjects.nopCommerce.user.UserRegisterPageObject;
+import pageObjects.nopCommerce.user.UserRewardpointsPage;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Level_12_Register_Login_Assert_Verify extends BaseTest {
+
+	private WebDriver driver;
+	RegisterPageObject registerPage;
+	private String projectPath = System.getProperty("user.dir");
+
+	@Parameters("browser")
+	@BeforeClass
+	public void beforeClass(String browserName) {
+
+		driver = getBrowserDriver(browserName);
+		registerPage = PageGeneratorManager.getRegisterPage(driver);
+
+	}
+
+	@Test
+	public void Register_01_Element_Displayed() {
+		// fail lần 1
+		verifyFalse(registerPage.isEmailTextboxDisplayed());
+		registerPage.enterToEmailTextbox("cam@gmail.com");
+		verifyTrue(registerPage.isConfirmEmailTextboxDisplayed());
+
+		registerPage.enterToEmailTextbox("");
+		registerPage.sleepInsecond(3);
+
+		// fail lần 2
+		verifyTrue(registerPage.isConfirmEmailTextboxDisplayed());
+
+		verifyTrue(registerPage.isConfirmEmailTextboxUnDisplayed());
+
+		// fail lần 3
+		verifyTrue(registerPage.isButtonLoginDisplayed());
+
+		verifyTrue(registerPage.isButtonLoginUnDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
